43cf99011852141a8a8d108e4cb2bd07a9b2f4b6
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-05-22 22:36:04 +0600

    server deploy on vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const roommateCollection = client
       .db("data-collection")
       .collection("RoommateCollection");
